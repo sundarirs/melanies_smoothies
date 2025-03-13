84178689cb75e79d467bbd2f8d06735c9d558cbf
sundarirs <sundarirs@gmail.com>
2025-03-13 19:58:41 +0530

    Update streamlit_app.py for response

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ if ingredients_list:
 #New section to display smoothie fruit nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
